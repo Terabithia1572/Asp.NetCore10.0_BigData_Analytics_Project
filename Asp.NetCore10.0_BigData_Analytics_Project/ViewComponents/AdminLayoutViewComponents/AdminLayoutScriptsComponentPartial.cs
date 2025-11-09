b38b007be6d392a02b7d7d351965917776b28a4a
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-11-09 15:10:02 +0300

    8# Script Component Partial + RenderBody Process Added

---
+++ Asp.NetCore10.0_BigData_Analytics_Project/ViewComponents/AdminLayoutViewComponents/AdminLayoutScriptsComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore10._0_BigData_Analytics_Project.ViewComponents.AdminLayoutViewComponents
+{
+    public class AdminLayoutScriptsComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
