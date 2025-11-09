50fc5180b71162b18a60ca6c7eb76b061f4082a3
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-11-09 14:54:45 +0300

    7#Sidebar Component Partial Process Added

---
+++ Asp.NetCore10.0_BigData_Analytics_Project/ViewComponents/SidebarViewComponents/_AdminLayoutSidebarComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore10._0_BigData_Analytics_Project.ViewComponents.SidebarViewComponents
+{
+    public class _AdminLayoutSidebarComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
