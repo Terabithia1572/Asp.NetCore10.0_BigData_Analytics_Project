a2569ff855cc99ebbb487e74238635afca7b3cf9
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-11-16 15:41:29 +0300

    42# Map Preparation in Dashboard

---
+++ Asp.NetCore10.0_BigData_Analytics_Project/DTOs/CountryReportDTO.cs
@@ -0,0 +1,12 @@
+﻿namespace Asp.NetCore10._0_BigData_Analytics_Project.DTOs
+{
+    public class CountryReportDTO
+    {
+        public string Country { get; set; }
+        public int Total2024 { get; set; }
+        public int Total2025 { get; set; }
+        public decimal ChangeRate { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
