bdd580ba2fe97ae2e8b9d8e12b8c3d80b44bf5c8
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-11-16 14:52:04 +0300

    40# Listing Today's Orders on the Dashboard Process Added

---
+++ Asp.NetCore10.0_BigData_Analytics_Project/ViewComponents/DashboardViewComponents/_DashboardTodayOrdersComponentPartial.cs
@@ -0,0 +1,41 @@
+﻿using Asp.NetCore10._0_BigData_Analytics_Project.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Asp.NetCore10._0_BigData_Analytics_Project.ViewComponents.DashboardViewComponents
+{
+    public class _DashboardTodayOrdersComponentPartial:ViewComponent
+    {
+        private readonly BigDataOrdersDBContext _context;
+
+        public _DashboardTodayOrdersComponentPartial(BigDataOrdersDBContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var today = DateTime.Today;
+            var tomorrow = today.AddDays(1);
+
+            var last10Orders = _context.Orders
+                .Include(o => o.Customer)
+                .Include(o => o.Product)
+                .Where(o => o.OrderDate >= today && o.OrderDate < tomorrow)
+                .OrderByDescending(o => o.OrderDate)
+                .Take(10)
+                .Select(o => new
+                {
+                    o.OrderID,
+                    o.Product.ProductName,
+                    CustomerName = o.Customer.CustomerName + " " + o.Customer.CustomerSurname,
+                    o.Quantity,
+                    o.PaymentMethod,
+                    o.OrderStatus,
+                    o.OrderDate
+                })
+                .ToList();
+            return View(last10Orders);
+        }
+    }
+}
