2a7eff61f9a46ee98087ecf25bd256627fe834a8
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-11-16 14:44:30 +0300

    39# Dashboard Page Process Added

---
+++ Asp.NetCore10.0_BigData_Analytics_Project/ViewComponents/DashboardViewComponents/_DashboardBlockHeaderComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore10._0_BigData_Analytics_Project.ViewComponents.DashboardViewComponents
+{
+    public class _DashboardBlockHeaderComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+
+    }
+}
