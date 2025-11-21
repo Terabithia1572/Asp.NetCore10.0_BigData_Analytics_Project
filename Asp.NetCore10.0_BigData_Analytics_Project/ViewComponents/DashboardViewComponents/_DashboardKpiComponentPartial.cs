c8f0350fb63e5dc303a71ea9db1e857f24bb9174
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-11-21 22:50:23 +0300

    48# Key Performance Indicator Process Added

---
+++ Asp.NetCore10.0_BigData_Analytics_Project/ViewComponents/DashboardViewComponents/_DashboardKpiComponentPartial.cs
@@ -0,0 +1,18 @@
+﻿using Asp.NetCore10._0_BigData_Analytics_Project.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore10._0_BigData_Analytics_Project.ViewComponents.DashboardViewComponents
+{
+    public class _DashboardKpiComponentPartial : ViewComponent
+    {
+        private readonly BigDataOrdersDBContext _context;
+        public _DashboardKpiComponentPartial(BigDataOrdersDBContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
