d507dfe7a48c90d23748685a0af6aeb8c1b620ba
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-11-24 23:27:48 +0300

    58# Writing Sub-Statistics

---
+++ Asp.NetCore10.0_BigData_Analytics_Project/ViewComponents/DashboardViewComponents/_DashboardSubStatisticsComponentPartial.cs
@@ -0,0 +1,25 @@
+﻿using Asp.NetCore10._0_BigData_Analytics_Project.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore10._0_BigData_Analytics_Project.ViewComponents.DashboardViewComponents
+{
+    public class _DashboardSubStatisticsComponentPartial:ViewComponent
+    {
+        private readonly BigDataOrdersDBContext _context;
+        public _DashboardSubStatisticsComponentPartial(BigDataOrdersDBContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            ViewBag.CategoryCount = _context.Categories.Count();
+            ViewBag.ProductCount = _context.Products.Count();
+            ViewBag.CustomerCount = _context.Customers.Count();
+            ViewBag.OrderCount = _context.Orders.Count();
+            ViewBag.CountryCount = _context.Customers.Select(x => x.CustomerCountry).Distinct().Count();
+            ViewBag.CityCount = _context.Customers.Select(x => x.CustomerCity).Distinct().Count();
+
+            return View();
+        }
+    }
+}
