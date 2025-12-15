96709261f02c6d1b1483e4b8a5221f253e54da41
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-12-15 21:51:31 +0300

    72# Payment Method Prediction Application Process Added

--- Asp.NetCore10.0_BigData_Analytics_Project/Program.cs
+++ Asp.NetCore10.0_BigData_Analytics_Project/Program.cs
@@ -1,5 +1,6 @@
 using Asp.NetCore10._0_BigData_Analytics_Project.Context;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.ML;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,8 @@ builder.Services.AddDbContext<BigDataOrdersDBContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("BigDataOrdersConnectionString")); //Veritabaný baðlantý dizesi
 });
+
+builder.Services.AddScoped<MLContext>(); // MLContext servisi ekleme
 
 var app = builder.Build();
 
