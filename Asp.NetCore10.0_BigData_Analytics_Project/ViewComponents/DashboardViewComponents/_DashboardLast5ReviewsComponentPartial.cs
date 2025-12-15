c71ae1a44f09c0ae563aa5273f428095b1e6f295
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-12-15 20:42:46 +0300

    62# Listing the last 5 reviews on the dashboard Process Added

---
+++ Asp.NetCore10.0_BigData_Analytics_Project/ViewComponents/DashboardViewComponents/_DashboardLast5ReviewsComponentPartial.cs
@@ -0,0 +1,20 @@
+﻿using Asp.NetCore10._0_BigData_Analytics_Project.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Asp.NetCore10._0_BigData_Analytics_Project.ViewComponents.DashboardViewComponents
+{
+    public class _DashboardLast5ReviewsComponentPartial:ViewComponent
+    {
+        private readonly BigDataOrdersDBContext _context;
+        public _DashboardLast5ReviewsComponentPartial(BigDataOrdersDBContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var values = _context.Reviews.OrderByDescending(x => x.ReviewID).Include(y => y.Customer).Include(z => z.Product).Take(5).ToList();
+            return View(values);
+        }
+    }
+}
