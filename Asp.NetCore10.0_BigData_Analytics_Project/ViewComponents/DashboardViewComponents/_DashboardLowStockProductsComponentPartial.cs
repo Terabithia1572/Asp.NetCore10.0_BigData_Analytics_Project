400789ad5c19e133ace4e480ead13458fe061457
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-12-15 20:49:29 +0300

    63# Listing Products with Critical Stock Status on the Dashboard Process Added

---
+++ Asp.NetCore10.0_BigData_Analytics_Project/ViewComponents/DashboardViewComponents/_DashboardLowStockProductsComponentPartial.cs
@@ -0,0 +1,20 @@
+﻿using Asp.NetCore10._0_BigData_Analytics_Project.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Asp.NetCore10._0_BigData_Analytics_Project.ViewComponents.DashboardViewComponents
+{
+    public class _DashboardLowStockProductsComponentPartial:ViewComponent
+    {
+        private readonly BigDataOrdersDBContext _context;
+        public _DashboardLowStockProductsComponentPartial(BigDataOrdersDBContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var values = _context.Products.Include(x => x.Category).Where(y => y.StockQuantity <= 9).Take(15).ToList();
+            return View(values);
+        }
+    }
+}
