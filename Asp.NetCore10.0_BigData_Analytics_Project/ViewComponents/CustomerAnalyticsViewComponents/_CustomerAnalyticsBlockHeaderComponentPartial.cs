22fda72872a925b4f36ad7dd693d787189fe69e7
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-12-16 20:42:26 +0300

    79# Customer Statistics Cards Process Added

---
+++ Asp.NetCore10.0_BigData_Analytics_Project/ViewComponents/CustomerAnalyticsViewComponents/_CustomerAnalyticsBlockHeaderComponentPartial.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore10._0_BigData_Analytics_Project.ViewComponents.CustomerAnalyticsViewComponents
+{
+    public class _CustomerAnalyticsBlockHeaderComponentPartial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
---
+++ Asp.NetCore10.0_BigData_Analytics_Project/ViewComponents/CustomerAnalyticsViewComponents/_CustomerAnalyticsMainStatisticsComponentPartial.cs
@@ -0,0 +1,20 @@
+﻿using Asp.NetCore10._0_BigData_Analytics_Project.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore10._0_BigData_Analytics_Project.ViewComponents.CustomerAnalyticsViewComponents
+{
+    public class _CustomerAnalyticsMainStatisticsComponentPartial:ViewComponent
+    {
+        private readonly BigDataOrdersDBContext _context;
+
+        public _CustomerAnalyticsMainStatisticsComponentPartial(BigDataOrdersDBContext context)
+        {
+            _context = context;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
