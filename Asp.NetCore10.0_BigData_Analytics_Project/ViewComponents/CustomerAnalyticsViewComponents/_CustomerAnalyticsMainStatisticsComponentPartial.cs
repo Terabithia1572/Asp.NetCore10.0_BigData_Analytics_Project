ccfac78da670de4d05a23db0d6c55dd917a1c360
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-12-16 20:47:58 +0300

    80# Churn Customers and Other Statistics

--- Asp.NetCore10.0_BigData_Analytics_Project/ViewComponents/CustomerAnalyticsViewComponents/_CustomerAnalyticsMainStatisticsComponentPartial.cs
+++ Asp.NetCore10.0_BigData_Analytics_Project/ViewComponents/CustomerAnalyticsViewComponents/_CustomerAnalyticsMainStatisticsComponentPartial.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace Asp.NetCore10._0_BigData_Analytics_Project.ViewComponents.CustomerAnalyticsViewComponents
 {
-    public class _CustomerAnalyticsMainStatisticsComponentPartial:ViewComponent
+    public class _CustomerAnalyticsMainStatisticsComponentPartial : ViewComponent
     {
         private readonly BigDataOrdersDBContext _context;
 
@@ -14,6 +14,28 @@ namespace Asp.NetCore10._0_BigData_Analytics_Project.ViewComponents.CustomerAnal
 
         public IViewComponentResult Invoke()
         {
+            var totalCustomerCount = _context.Customers.Count();
+
+            ViewBag.TotalCustomerCount = totalCustomerCount;
+
+            var totalOrderCount = _context.Orders.Count();
+
+            var averageOrderPerCustomerCount = totalOrderCount / totalCustomerCount;
+
+            ViewBag.AverageOrderPerCustomerCount = averageOrderPerCustomerCount;
+
+            var threeMonthsAgo = DateTime.Now.AddMonths(-3);
+
+            var activeCustomerCount = _context.Orders.Where(o => o.OrderDate >= threeMonthsAgo).Select(o => o.CustomerID).Distinct().Count();
+
+            ViewBag.ActiveCustomerCount = activeCustomerCount;
+
+            var sixMonthsAgo = DateTime.Now.AddMonths(-6);
+
+            var inactiveCustomerCount = _context.Customers.Count(c => !_context.Orders.Any(o => o.CustomerID == c.CustomerID && o.OrderDate >= sixMonthsAgo));
+
+            ViewBag.InactiveCustomerCount = inactiveCustomerCount;
+
             return View();
         }
     }
