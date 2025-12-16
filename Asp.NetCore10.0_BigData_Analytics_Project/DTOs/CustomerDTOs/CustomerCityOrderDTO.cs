466707bbe59041052cd4495e2bf9bdc852575055
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-12-16 21:18:42 +0300

    87# Graph by City of Order Placement Process Added

---
+++ Asp.NetCore10.0_BigData_Analytics_Project/DTOs/CustomerDTOs/CustomerCityOrderDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Asp.NetCore10._0_BigData_Analytics_Project.DTOs.CustomerDTOs
+{
+    public class CustomerCityOrderDTO
+    {
+        public string City { get; set; }
+        public int OrderCount { get; set; }
+        public double Percentage { get; set; }
+    }
+}
---
+++ Asp.NetCore10.0_BigData_Analytics_Project/ViewComponents/CustomerAnalyticsViewComponents/_CustomerAnalyticsCustomerCitiesComponentPartial.cs
@@ -0,0 +1,36 @@
+﻿using Asp.NetCore10._0_BigData_Analytics_Project.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Asp.NetCore10._0_BigData_Analytics_Project.ViewComponents.CustomerAnalyticsViewComponents
+{
+    public class _CustomerAnalyticsCustomerCitiesComponentPartial:ViewComponent
+    {
+        private readonly BigDataOrdersDBContext _context;
+        public _CustomerAnalyticsCustomerCitiesComponentPartial(BigDataOrdersDBContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var topCities = _context.Orders
+                   .Include(o => o.Customer)
+                   .Where(o => o.Customer.CustomerCity != null)
+                   .GroupBy(o => o.Customer.CustomerCity)
+                   .Select(g => new
+                   {
+                       CityName = g.Key,
+                       OrderCount = g.Count()
+                   })
+                   .OrderByDescending(x => x.OrderCount)
+                   .Take(7)
+                   .ToList();
+
+            // Chart.js'e göndermek için ViewBag veya ViewModel kullanılabilir
+            ViewBag.CityLabels = topCities.Select(x => x.CityName).ToList();
+            ViewBag.CityValues = topCities.Select(x => x.OrderCount).ToList();
+
+            return View();
+        }
+    }
+}
