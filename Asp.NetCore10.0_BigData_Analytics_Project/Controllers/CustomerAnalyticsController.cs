33d5592b0022430c8a2a4ceff98a243c6096a83f
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-12-16 20:34:23 +0300

    78# Customer Analytics operations have been added

---
+++ Asp.NetCore10.0_BigData_Analytics_Project/Controllers/CustomerAnalyticsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore10._0_BigData_Analytics_Project.Controllers
+{
+    public class CustomerAnalyticsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
