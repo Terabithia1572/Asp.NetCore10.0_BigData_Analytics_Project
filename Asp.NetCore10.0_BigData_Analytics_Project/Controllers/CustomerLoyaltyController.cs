ae08feae7d9caed34eefb9b58c995aa70a655dfa
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-12-17 22:31:22 +0300

    91# Italy Loyalty Score Method

---
+++ Asp.NetCore10.0_BigData_Analytics_Project/Controllers/CustomerLoyaltyController.cs
@@ -0,0 +1,41 @@
+﻿using Asp.NetCore10._0_BigData_Analytics_Project.Context;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Asp.NetCore10._0_BigData_Analytics_Project.Controllers
+{
+    public class CustomerLoyaltyController : Controller
+    {
+        private readonly BigDataOrdersDBContext _context;
+
+        public CustomerLoyaltyController(BigDataOrdersDBContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult ItalyLoyaltyScore()
+        {
+            var loyaltyScores=_context.Customers // Müşteriler
+                .Include(c=>c.Orders) // Siparişler
+                .ThenInclude(o=>o.Product) // Ürünler
+                .Where(c=>c.CustomerCountry=="İtalya") // İtalya'daki müşteriler
+                .Select(c=> new // Sadakat Skoru Hesaplama
+                {
+                    CustoemrName=c.CustomerName+" "+c.CustomerSurname, // Müşteri Adı
+                    TotalOrders =c.Orders.Count(), // Toplam Sipariş Sayısı
+                    TotalSpent =c.Orders.Sum(o=>o.Quantity* o.Product.UnitPrice), // Toplam Harcama
+                    LastOrderDate =c.Orders.Max(o=>(DateTime?)o.OrderDate), // Son Sipariş Tarihi
+                })
+                .AsEnumerable() // Bellek içi işleme için
+                .Select(x => // Sadakat Skoru Hesaplama
+                {
+                    var daySinceLastOrder = x.LastOrderDate.HasValue  // Son Sipariş Tarihinden Bu Yana Geçen Gün Sayısı
+                    ? (DateTime.Now - x.LastOrderDate.Value).TotalDays  // Eğer sipariş varsa
+                    : double.MaxValue; // Eğer sipariş yoksa maksimum değer
+
+
+                })
+            return View();
+        }
+    }
+}
---
+++ Asp.NetCore10.0_BigData_Analytics_Project/DTOs/LoyaltyDTOs/LoyaltyScoreDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Asp.NetCore10._0_BigData_Analytics_Project.DTOs.LoyaltyDTOs
+{
+    public class LoyaltyScoreDTO
+    {
+        public string CustomerName { get; set; } // Müşteri Adı
+        public int TotalOrders { get; set; } // Toplam Sipariş Sayısı
+        public double TotalSpent { get; set; } // Toplam Harcama
+        public DateTime? LastOrderDate { get; set; } // Son Sipariş Tarihi
+        public double LoyaltyScore { get; set; } // Sadakat Skoru
+    }
+}
