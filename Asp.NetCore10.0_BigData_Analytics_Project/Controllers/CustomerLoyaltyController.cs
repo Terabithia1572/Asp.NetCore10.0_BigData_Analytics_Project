8bd0dd86fad511c9c5ec9a8047a63951b96ce845
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-12-17 22:34:07 +0300

    92# Frequency Monetary and Recency Score Calculation Process Added

--- Asp.NetCore10.0_BigData_Analytics_Project/Controllers/CustomerLoyaltyController.cs
+++ Asp.NetCore10.0_BigData_Analytics_Project/Controllers/CustomerLoyaltyController.cs
@@ -1,4 +1,5 @@
 ﻿using Asp.NetCore10._0_BigData_Analytics_Project.Context;
+using Asp.NetCore10._0_BigData_Analytics_Project.DTOs.LoyaltyDTOs;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -15,27 +16,65 @@ namespace Asp.NetCore10._0_BigData_Analytics_Project.Controllers
 
         public IActionResult ItalyLoyaltyScore()
         {
-            var loyaltyScores=_context.Customers // Müşteriler
-                .Include(c=>c.Orders) // Siparişler
-                .ThenInclude(o=>o.Product) // Ürünler
-                .Where(c=>c.CustomerCountry=="İtalya") // İtalya'daki müşteriler
-                .Select(c=> new // Sadakat Skoru Hesaplama
-                {
-                    CustoemrName=c.CustomerName+" "+c.CustomerSurname, // Müşteri Adı
-                    TotalOrders =c.Orders.Count(), // Toplam Sipariş Sayısı
-                    TotalSpent =c.Orders.Sum(o=>o.Quantity* o.Product.UnitPrice), // Toplam Harcama
-                    LastOrderDate =c.Orders.Max(o=>(DateTime?)o.OrderDate), // Son Sipariş Tarihi
-                })
-                .AsEnumerable() // Bellek içi işleme için
-                .Select(x => // Sadakat Skoru Hesaplama
-                {
-                    var daySinceLastOrder = x.LastOrderDate.HasValue  // Son Sipariş Tarihinden Bu Yana Geçen Gün Sayısı
-                    ? (DateTime.Now - x.LastOrderDate.Value).TotalDays  // Eğer sipariş varsa
-                    : double.MaxValue; // Eğer sipariş yoksa maksimum değer
+            var loyaltScores = _context.Customers
+                 .Include(c => c.Orders)
+                 .ThenInclude(o => o.Product)
+                 .Where(c => c.CustomerCountry == "İtalya" && c.CustomerCity == "Parma" || c.CustomerCity == "Bologna" || c.CustomerCity == "Como" || c.CustomerCity == "Siena" || c.CustomerCity == "Verona" || c.CustomerCity == "Bergamo" || c.CustomerCity == "Bari" || c.CustomerCity == "Venedik")
+                 .Select(c => new
+                 {
+                     CustomerName = c.CustomerName + " " + c.CustomerSurname,
+                     TotalOrders = c.Orders.Count(),
+                     TotalSpent = c.Orders.Sum(o => o.Quantity * o.Product.UnitPrice),
+                     LastOrderDate = c.Orders.Max(o => (DateTime?)o.OrderDate)
+                 })
+                 .AsEnumerable()
+                 .Select(x =>
+                 {
+                     var daySinceLastOrder = (x.LastOrderDate.HasValue)
+                         ? (DateTime.Now - x.LastOrderDate.Value).TotalDays
+                         : double.MaxValue;
 
+                     double recenyScore = daySinceLastOrder switch
+                     {
+                         <= 30 => 100,
+                         <= 90 => 75,
+                         <= 180 => 50,
+                         <= 365 => 25,
+                         _ => 10
+                     };
 
-                })
-            return View();
+                     double frequencyScore = x.TotalOrders switch
+                     {
+                         >= 20 => 100,
+                         >= 10 => 80,
+                         >= 5 => 60,
+                         >= 2 => 40,
+                         1 => 20,
+                         _ => 10
+                     };
+
+                     double monetaryScore = x.TotalSpent switch
+                     {
+                         >= 5000 => 100,
+                         >= 3000 => 80,
+                         >= 1000 => 60,
+                         >= 500 => 40,
+                         >= 100 => 20,
+                         _ => 10
+                     };
+
+                     double loyaltyScore = (recenyScore * 0.4) + (frequencyScore * 0.3) + (monetaryScore * 0.3);
+
+                     return new LoyaltyScoreDTO
+                     {
+                         CustomerName = x.CustomerName,
+                         TotalOrders = x.TotalOrders,
+                         TotalSpent = Math.Round(x.TotalSpent, 2),
+                         LastOrderDate = x.LastOrderDate,
+                         LoyaltyScore = Math.Round(loyaltyScore, 2)
+                     };
+                 }).OrderByDescending(x => x.LoyaltyScore).ToList();
+            return View(loyaltScores);
         }
     }
 }
