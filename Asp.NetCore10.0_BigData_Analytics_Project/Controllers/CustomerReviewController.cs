6eece2047e8524e396aaba795b01d3462e434fe1
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-12-18 21:10:38 +0300

    117# The process of preparing components for review has been added.

---
+++ Asp.NetCore10.0_BigData_Analytics_Project/Controllers/CustomerReviewController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore10._0_BigData_Analytics_Project.Controllers
+{
+    public class CustomerReviewController : Controller
+    {
+        public IActionResult CustomerReviewWithOpenAI()
+        {
+            return View();
+        }
+    }
+}
---
+++ Asp.NetCore10.0_BigData_Analytics_Project/ViewComponents/CustomerReviewViewComponents/_CustomerReviewWithOpenAIAnalysisComponentPartial.cs
@@ -0,0 +1,20 @@
+﻿using Asp.NetCore10._0_BigData_Analytics_Project.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore10._0_BigData_Analytics_Project.ViewComponents.CustomerReviewViewComponents
+{
+    public class _CustomerReviewWithOpenAIAnalysisComponentPartial : ViewComponent
+    {
+        private readonly BigDataOrdersDBContext _context;
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _CustomerReviewWithOpenAIAnalysisComponentPartial(BigDataOrdersDBContext context, IHttpClientFactory httpClientFactory)
+        {
+            _context = context;
+            _httpClientFactory = httpClientFactory;
+        }
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
