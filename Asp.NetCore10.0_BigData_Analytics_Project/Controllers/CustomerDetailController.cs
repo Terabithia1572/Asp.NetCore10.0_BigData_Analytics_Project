363660a8adae60d3423a79d7fa8f085a5305c12d
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-12-18 19:57:44 +0300

    105# Customer Details Page features have been added.

---
+++ Asp.NetCore10.0_BigData_Analytics_Project/Controllers/CustomerDetailController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore10._0_BigData_Analytics_Project.Controllers
+{
+    public class CustomerDetailController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
