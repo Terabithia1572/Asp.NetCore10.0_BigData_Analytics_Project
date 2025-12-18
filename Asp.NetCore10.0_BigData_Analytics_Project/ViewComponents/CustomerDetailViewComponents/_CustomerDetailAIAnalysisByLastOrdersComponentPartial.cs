050713b843442e0e00578c173e6f657dcdf0d5d9
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-12-18 20:18:34 +0300

    108# Preparation for Artificial Intelligence Integration Process Added

---
+++ Asp.NetCore10.0_BigData_Analytics_Project/ViewComponents/CustomerDetailViewComponents/_CustomerDetailAIAnalysisByLastOrdersComponentPartial.cs
@@ -0,0 +1,21 @@
+﻿using Asp.NetCore10._0_BigData_Analytics_Project.Context;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp.NetCore10._0_BigData_Analytics_Project.ViewComponents.CustomerDetailViewComponents
+{
+    public class _CustomerDetailAIAnalysisByLastOrdersComponentPartial:ViewComponent
+    {
+        private readonly BigDataOrdersDBContext _context;
+        private readonly IHttpClientFactory _httpClientFactory;
+        public _CustomerDetailAIAnalysisByLastOrdersComponentPartial(BigDataOrdersDBContext context, IHttpClientFactory httpClientFactory)
+        {
+            _context = context;
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
