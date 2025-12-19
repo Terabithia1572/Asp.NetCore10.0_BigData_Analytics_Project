91972a43692f951bdcdd212aaaccba536dad4cac
Terabithia1572 <71444461+Terabithia1572@users.noreply.github.com>
2025-12-19 20:35:02 +0300

    119# Message Entity Process Added

---
+++ Asp.NetCore10.0_BigData_Analytics_Project/Entities/Message.cs
@@ -0,0 +1,13 @@
+﻿namespace Asp.NetCore10._0_BigData_Analytics_Project.Entities
+{
+    public class Message
+    {
+        public int MessageID { get; set; }
+        public int CustomerID { get; set; }
+        public Customer Customer { get; set; }
+        public string MessageSubject { get; set; }
+        public string MessageText { get; set; }
+        public string SentimentLabel { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
